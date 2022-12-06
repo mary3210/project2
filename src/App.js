df98e2d090169569f31c760137cc8f87cfaba99d
mary3210 <maryrodri3210@gmail.com>
2022-12-06 10:58:54 -0600

    Created CharacterImages component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import React from "react";
+import CharacterImages from './Components/CharacterImages';
+
+
+
 
 function App() {
   return (
+
     <div className="App">
-     <h1>Concept App</h1>
+      <header>
+      <h1>Rick and Morty App</h1>
+      </header>
+    <main>
+      <CharacterImages />
+    </main>
+
     </div>
   );
 }
