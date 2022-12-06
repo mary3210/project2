5ac2550bb9cf8160599b5a164fc471421c49dbaa
mary3210 <maryrodri3210@gmail.com>
2022-12-06 11:35:33 -0600

    Added characterImages.map

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@
 import './App.css';
 import React from "react";
 import CharacterImages from './Components/CharacterImages';
-
-
-
+import {Route, Routes} from "react-router-dom";
+import ReactDom from 'react-dom'
 
 function App() {
   return (
-
+    <>
     <div className="App">
       <header>
       <h1>Rick and Morty App</h1>
@@ -18,6 +17,7 @@ function App() {
     </main>
 
     </div>
+    </>
   );
 }
 
