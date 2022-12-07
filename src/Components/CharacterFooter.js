00bba7a8d5ca36bfdd28888fb6521240a392e546
mary3210 <maryrodri3210@gmail.com>
2022-12-07 12:58:49 -0600

    Added a nagivation link to previous character in CharacterFooter.js

--- src/Components/CharacterFooter.js
+++ src/Components/CharacterFooter.js
@@ -3,11 +3,13 @@ import { useParams } from 'react-router-dom'
 
 export default function CharacterFooter(){
     const {id} = useParams()
-    const url =`/details/${Number(id) + Number(1)}`
+    const url1 =`/details/${Number(id) + Number(1)}`
+    const url2 =`/details/${Number(id) - Number(1)}`
     return (
         <div className="characterFooter">
             <p>Im the footer that works </p>
-            <h1> <a href={url}> See Next Character </a></h1>
+            <h1> <a href={url1}> See Next Character </a></h1>
+            <h1> <a href={url2}> See Previous Character </a></h1>
         </div>
     )
 }
