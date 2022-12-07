b72459a74c03699a1198a302fc2eb57b25859032
mary3210 <maryrodri3210@gmail.com>
2022-12-07 12:55:20 -0600

    Added a nagivation link in the CharacterFooter.js

--- src/Components/CharacterFooter.js
+++ src/Components/CharacterFooter.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import { useParams } from 'react-router-dom'
 
 export default function CharacterFooter(){
+    const {id} = useParams()
+    const url =`/details/${Number(id) + Number(1)}`
     return (
         <div className="characterFooter">
             <p>Im the footer that works </p>
+            <h1> <a href={url}> See Next Character </a></h1>
         </div>
     )
 }
