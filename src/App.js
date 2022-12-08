4878fed43a3c1bc5fbe766e992a7fba86a2a762c
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-12-08 15:43:58 -0800

    Added background color, added CharacterNavbar.css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from "react";
 import CharacterImages from './Components/CharacterImages';
 import CharacterDetails from './Components/CharacterDetails';
 import CharacterFooter from './Components/CharacterFooter';
-import CharacterNavbar from './Components/CharacterNavbar'
-import CharacterSearch from './Components/CharacterSearch'
+import CharacterNavbar from './Components/CharacterNavbar';
+import CharacterSearch from './Components/CharacterSearch';
 import {Route, Routes} from "react-router-dom";
-import ReactDom from 'react-dom'
-import { useState } from 'react'
+import ReactDom from 'react-dom';
+import { useState } from 'react';
+// import './CharacterNavbar.css';
+
+
 
 
 function App() {
