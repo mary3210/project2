be53ab936c014b77cddfce70e09518ac148cdb97
mary3210 <maryrodri3210@gmail.com>
2022-12-09 13:42:48 -0600

    Removed CharacterImages CharacterList components,  app.css and cleaned up code

--- src/Components/CharacterDetails.js
+++ src/Components/CharacterDetails.js
@@ -1,33 +1,33 @@
 import React from "react";
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import CharacterFooter from "./CharacterFooter";
-import './CharacterDetails.css'
+import "./CharacterDetails.css";
 
-export default function CharacterDetails(){
-    const {id} = useParams()
-    const url =`https://rickandmortyapi.com/api/character/${id}`
-    const [character, setCharacter] = useState({})
-    useEffect(() => {
-        fetch(url)
-        .then((res) => res.json())
-        .then((json) => {
-            setCharacter(json);
-        })
-        .catch(console.err);
-    },[]);
-    
-    return (
-        <div className="detailsContainer">
-            
-            <img className="image1" src={character.image} alt={character.name}/>
-           
-            
-            <p>Hello, my name is {character.name}. I am currenltly {character.status}. I am a {character.gender} {character.species}. I am from planet {character.origin?.name}.</p>
-            <footer>
-            <CharacterFooter />
-            </footer>
-        </div>
-    )
+export default function CharacterDetails() {
+  const { id } = useParams();
+  const url = `https://rickandmortyapi.com/api/character/${id}`;
+  const [character, setCharacter] = useState({});
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        setCharacter(json);
+      })
+      .catch(console.err);
+  }, []);
 
+  return (
+    <div className="detailsContainer">
+      <img className="image1" src={character.image} alt={character.name} />
+      <p>
+        Hello, my name is {character.name}. I am currenltly {character.status}.
+        I am a {character.gender} {character.species}. I am from planet{" "}
+        {character.origin?.name}.
+      </p>
+      <footer>
+        <CharacterFooter />
+      </footer>
+    </div>
+  );
 }
